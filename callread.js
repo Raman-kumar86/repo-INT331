6194992064d61e0e8776f277c569f8834ca55ad1
Raman kumar <146643597+Raman-kumar86@users.noreply.github.com>
2025-10-30 13:36:45 +0530

    Add files via upload

--- callread.js
+++ callread.js
@@ -3,6 +3,6 @@ fs.readFile('demo2.txt','utf-8',function(err,data){
     if(err){
         console.log("Error: ",err);
     }else{
-        console.log("File content: ", data);
+        console.log("File content: qkdjnjkadsnlojn asdoljn ", data);
     }
 });
