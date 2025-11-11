87deb77db536b7172e5abfff76c4bf140e6f6bdb
vijay <vijay@gmail.com>
2025-11-11 12:41:35 +0530

    final done

--- callread.js
+++ callread.js
@@ -3,6 +3,6 @@ fs.readFile('demo2.txt','utf-8',function(err,data){
     if(err){
         console.log("Error: ",err);
     }else{
-        console.log("File content: qkdjnjkadsnlojn asdoljn ", data);
+        console.log("File content: hello ", data);
     }
 });
